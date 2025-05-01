6323326e9efa99f894c24da244cf7dcc36df2db8
Mateus Ribeiro <ribeiro211097@gmail.com>
2025-05-01 16:14:28 -0300

    Cria escolha personagens

--- src/menu.h
+++ src/menu.h
@@ -1,9 +1,17 @@
 #ifndef MENU_H
 #define MENU_H
 #include <stdbool.h>
+
 void InitMenu(void);
 void UpdateMenu(void);
 void DrawMenu(void);
 bool MenuStartGame(void);
 void UnloadMenu(void);
+
+// Função para obter o índice do personagem selecionado após a confirmação
+int MenuSelectedCharacter(void);
+
+// NOVO: função para obter o NOME do personagem selecionado após a confirmação
+const char* MenuSelectedCharacterName(void);
+
 #endif
