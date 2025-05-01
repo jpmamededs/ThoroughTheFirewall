f9c3cb5af2509cfffddf6a9de057f7f6bddc88e8
Mateus Ribeiro <ribeiro211097@gmail.com>
2025-05-01 11:51:15 -0300

    Refatora main e criar o pular cuts

---
+++ src/cutscenes.h
@@ -0,0 +1,11 @@
+#ifndef CUTSCENES_H
+#define CUTSCENES_H
+
+#include "raylib.h"
+
+void InitCutscenes(void);
+void UpdateCutscenes(void);
+void DrawCutscenes(void);
+void UnloadCutscenes(void);
+
+#endif
