4f34047b872ad01b3c070e0bcab048d5a48ee591
Mateus Ribeiro <ribeiro211097@gmail.com>
2025-05-16 09:38:41 -0300

    ajustes pra mamede

--- src/main.c
+++ src/main.c
@@ -112,7 +112,7 @@ int main(void)
     extern bool interrogatorioFinalizado;
 
     // DEBUG DE SELEÇÃO DO NOME
-    strncpy(gSelectedCharacterName, "Levi", MAX_PLAYER_NAME);
+    strncpy(gSelectedCharacterName, "Alice", MAX_PLAYER_NAME);
     gSelectedCharacterName[MAX_PLAYER_NAME-1] = '\0';
 
     while (!WindowShouldClose())
